c49c0ee9ad1661b61c42642c7aae349f94c441d0
Jiahao XU <Jiahao_XU@outlook.com>
2021-02-20 17:13:23 +1100

    Declare struct mem_size_t && Impl its formatter
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ src/mem_size_t.cc
@@ -0,0 +1,116 @@
+#include <cassert>
+
+#include "mem_size_t.hpp"
+
+using formatter = fmt::formatter<swaystatus::mem_size_t>;
+
+auto formatter::parse(format_parse_context &ctx) -> format_parse_context_it
+{
+    auto it = ctx.begin(), end = ctx.end();
+    if (it == end)
+        return it;
+
+    switch (*it) {
+        default:
+            FMT_THROW(format_error("invalid format"));
+
+        case 'A':
+        case 'K':
+        case 'M':
+        case 'G':
+        case 'T':
+        case 'P':
+        case 'E':
+        case 'Z':
+        case 'Y':
+            presentation = *it;
+            if (++it == end)
+                return it;
+    }
+
+    if (*it == '}')
+        return it;
+
+    if (*it != ' ')
+        FMT_THROW(format_error("invalid format"));
+    ++it;
+
+    ctx.advance_to(it);
+    return fmt::formatter<size_t>::parse(ctx);
+}
+
+static const char* get_unit(size_t ratio)
+{
+    switch (ratio) {
+        case 1:
+            return "B";
+        case 2:
+            return "K";
+        case 3:
+            return "M";
+        case 4:
+            return "G";
+        case 5:
+            return "T";
+        case 6:
+            return "P";
+        case 7:
+            return "E";
+        case 8:
+            return "Z";
+        case 9:
+            return "Y";
+
+        default:
+            return "Invalid unit";
+    }
+}
+auto formatter::format_impl(size_t mem, const char *unit, format_context &ctx) -> format_context_it
+{
+    auto it = fmt::formatter<size_t>::format(mem, ctx);
+    return fmt::format_to(it, "{}", unit);
+}
+auto formatter::auto_format(size_t mem, format_context &ctx) -> format_context_it
+{
+    size_t ratio = 1;
+    for (; ratio < 9 && mem > 1024; ratio += 1)
+        mem /= 1024;
+
+    return format_impl(mem, get_unit(ratio), ctx);
+}
+auto formatter::format(const mem_size_t &sz, format_context &ctx) -> format_context_it
+{
+    size_t mem = sz.bytes;
+
+    switch (presentation) {
+        case 'K':
+            return format_impl(mem / 1024, "K", ctx);
+
+        case 'M':
+            return format_impl(mem / 1024 / 1024, "M", ctx);
+
+        case 'G':
+            return format_impl(mem / 1024 / 1024 / 1024, "G", ctx);
+
+        case 'T':
+            return format_impl(mem / 1024 / 1024 / 1024 / 1024, "T", ctx);
+
+        case 'P':
+            return format_impl(mem / 1024 / 1024 / 1024 / 1024 / 1024, "P", ctx);
+
+        case 'E':
+            return format_impl(mem / 1024 / 1024 / 1024 / 1024 / 1024 / 1024, "E", ctx);
+
+        case 'Z':
+            return format_impl(mem / 1024 / 1024 / 1024 / 1024 / 1024 / 1024 / 1024, "Z", ctx);
+
+        case 'Y':
+            return format_impl(mem / 1024 / 1024 / 1024 / 1024 / 1024 / 1024 / 1024 / 1024, "Y", ctx);
+
+        case 'A':
+            return auto_format(mem, ctx);
+
+        default:
+            assert(false);
+    }
+}
---
+++ src/mem_size_t.hpp
@@ -0,0 +1,34 @@
+#ifndef  __swaystatus_mem_size_t_H__
+# define __swaystatus_mem_size_t_H__
+
+# include <stddef.h>
+# include "dep/fmt/include/fmt/format.h"
+
+namespace swaystatus {
+struct mem_size_t {
+    size_t bytes;
+};
+}
+
+/**
+ * Expected replacement field for mem_size_t: "{A|K|M|G|T|P|E|Z|Y} {integer format}"
+ */
+template <>
+struct fmt::formatter<swaystatus::mem_size_t>: fmt::formatter<size_t> {
+    using format_parse_context = fmt::format_parse_context;
+    using format_context = fmt::format_context;
+    using mem_size_t = swaystatus::mem_size_t;
+
+    using format_parse_context_it = typename format_parse_context::iterator;
+    using format_context_it = typename format_context::iterator;
+
+    char presentation = 'A';
+
+    auto format_impl(size_t mem, const char *unit, format_context &ctx) -> format_context_it;
+    auto auto_format(size_t mem, format_context &ctx) -> format_context_it;
+
+    auto parse(format_parse_context &ctx) -> format_parse_context_it;
+    auto format(const mem_size_t &sz, format_context &ctx) -> format_context_it;
+};
+
+#endif
