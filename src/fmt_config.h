97122714b1064784c028ae6236f71ffdb77f595a
Jiahao XU <Jiahao_XU@outlook.com>
2021-02-22 08:49:07 +1100

    Debloat fmtlib: Disable floating point printing
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/fmt_config.h
+++ src/fmt_config.h
@@ -2,6 +2,18 @@
 # define FMT_HEADER_ONLY
 #endif
 
+#ifndef  FMT_USE_FLOAT
+# define FMT_USE_FLOAT 0
+#endif
+
+#ifndef  FMT_USE_DOUBLE
+# define FMT_USE_DOUBLE 0
+#endif
+
+#ifndef  FMT_USE_LONG_DOUBLE
+# define FMT_USE_LONG_DOUBLE 0
+#endif
+
 # ifndef  FMT_STATIC_THOUSANDS_SEPARATOR
 #  define FMT_STATIC_THOUSANDS_SEPARATOR ','
 # endif
