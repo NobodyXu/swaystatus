05ccdf4e7337a85f582147725df4c3102bafc1a5
Jiahao XU <Jiahao_XU@outlook.com>
2021-02-24 00:15:11 +1100

    Replace libnm with getifaddrs
    To remove dependency on linnm, which isn't used universally on every
    system.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ src/networking.cc
@@ -0,0 +1,341 @@
+#include <arpa/inet.h>
+#include <net/if.h>
+
+#include <cstring>
+#include <cinttypes>
+#include <climits>
+#include <cerrno>
+#include <algorithm>
+
+#include "utility.h"
+#include "fmt_utility.hpp"
+#include "Conditional.hpp"
+#include "mem_size_t.hpp"
+#include "networking.hpp"
+
+#include "dep/fmt/include/fmt/core.h"
+
+using swaystatus::Conditional;
+using swaystatus::mem_size_t;
+using swaystatus::find_end_of_format;
+
+namespace swaystatus {
+void ipv4_addrs::add(const struct sockaddr *src_addr) noexcept
+{
+    if (cnt == array.size())
+        return;
+
+    ++cnt;
+    auto &dest_ipv4_addr = array[cnt - 1];
+    auto &src_ipv4_addr = *reinterpret_cast<const struct sockaddr_in*>(src_addr);
+
+    dest_ipv4_addr = src_ipv4_addr.sin_addr;
+}
+void ipv4_addrs::reset() noexcept
+{
+    cnt = 0;
+}
+auto ipv4_addrs::begin() const noexcept -> const_iterator
+{
+    return array.begin();
+}
+auto ipv4_addrs::end() const noexcept -> const_iterator
+{
+    return begin() + cnt;
+}
+void ipv6_addrs::add(const struct sockaddr *src_addr) noexcept
+{
+    if (cnt == array.size())
+        return;
+
+    ++cnt;
+    auto &dest_ipv6_addr = array[cnt - 1];
+    auto &src_ipv6_addr = *reinterpret_cast<const struct sockaddr_in6*>(src_addr);
+
+    dest_ipv6_addr = src_ipv6_addr.sin6_addr;
+}
+void ipv6_addrs::reset() noexcept
+{
+    cnt = 0;
+}
+auto ipv6_addrs::begin() const noexcept -> const_iterator
+{
+    return array.begin();
+}
+auto ipv6_addrs::end() const noexcept -> const_iterator
+{
+    return begin() + cnt;
+}
+
+interface_stats Interface::get_empty_stats() noexcept
+{
+    interface_stats stats;
+    std::memset(&stats, 0, sizeof(stats));
+    return stats;
+}
+
+bool Interface::operator == (std::string_view interface_name) const noexcept
+{
+    return name == interface_name;
+}
+bool Interface::operator != (std::string_view interface_name) const noexcept
+{
+    return !(*this == interface_name);
+}
+
+void Interface::reset() noexcept
+{
+    /*
+     * name and flags will be overwriten anyway, so it's ok to not reset them.
+     */
+    stat = get_empty_stats();
+    ipv4_addrs_v.reset();
+    ipv6_addrs_v.reset();
+}
+
+auto Interfaces::operator [] (std::string_view name) noexcept -> iterator
+{
+    auto it = std::find(interfaces.begin(), interfaces.begin() + cnt, name);
+    if (it != interfaces.begin() + cnt)
+        return it;
+
+    if (cnt == interfaces.size())
+        return end();
+
+    ++cnt;
+    auto &interface = interfaces[cnt - 1];
+    interface.name = name;
+
+    return end() - 1;
+}
+
+bool Interfaces::is_empty() const noexcept
+{
+    return cnt == 0;
+}
+auto Interfaces::size() const noexcept -> std::size_t
+{
+    return cnt;
+}
+
+auto Interfaces::begin() noexcept -> iterator
+{
+    return interfaces.begin();
+}
+auto Interfaces::end() noexcept -> iterator
+{
+    return begin() + cnt;
+}
+
+auto Interfaces::begin() const noexcept -> const_iterator
+{
+    return interfaces.begin();
+}
+auto Interfaces::end() const noexcept -> const_iterator
+{
+    return begin() + cnt;
+}
+
+auto Interfaces::cbegin() const noexcept -> const_iterator
+{
+    return interfaces.cbegin();
+}
+auto Interfaces::cend() const noexcept -> const_iterator
+{
+    return cbegin() + cnt;
+}
+
+void Interfaces::clear() noexcept
+{
+    for (auto &interface: *this)
+        interface.reset();
+
+    cnt = 0;
+}
+} /* namespace swaystatus */
+
+using Interfaces_formatter = fmt::formatter<swaystatus::Interfaces>;
+
+auto Interfaces_formatter::parse(format_parse_context &ctx) -> format_parse_context_it
+{
+    auto it = ctx.begin(), end = ctx.end();
+    if (it == end)
+        return it;
+
+    end = find_end_of_format(ctx);
+
+    fmt_str = std::string_view{it, static_cast<std::size_t>(end - it)};
+
+    return end;
+}
+auto Interfaces_formatter::format(const Interfaces &interfaces, format_context &ctx) ->
+    format_context_it
+{
+    if (fmt_str.size() != 0) {
+        auto out = ctx.out();
+
+        std::size_t i = 0;
+        for (const auto &interface: interfaces) {
+            out = format_to(
+                out,
+                fmt_str,
+                fmt::arg("name", interface.name),
+#define FMT_FLAGS(name, var) \
+    fmt::arg(name, Conditional{static_cast<bool>(interface.flags & IFF_##var)})
+                FMT_FLAGS("has_broadcast_support", BROADCAST),
+                FMT_FLAGS("is_pointopoint",        POINTOPOINT),
+                FMT_FLAGS("has_no_arp_support",    NOARP),
+                FMT_FLAGS("is_in_promisc_mode",    PROMISC),
+                FMT_FLAGS("is_in_notrailers_mode", NOTRAILERS),
+                FMT_FLAGS("is_master",             MASTER),
+                FMT_FLAGS("is_slave",              SLAVE),
+                FMT_FLAGS("has_multicast_support", MULTICAST),
+                FMT_FLAGS("has_portsel_support",   PORTSEL),
+                FMT_FLAGS("is_automedia_active",   AUTOMEDIA),
+                FMT_FLAGS("is_dhcp",               DYNAMIC),
+                
+                fmt::arg(
+                    "HAS_UAPI_DEF_IF_NET_DEVICE_FLAGS_LOWER_UP_DORMANT_ECHO", 
+# if  __UAPI_DEF_IF_NET_DEVICE_FLAGS_LOWER_UP_DORMANT_ECHO
+                    Conditional{true}
+                ),
+                FMT_FLAGS("is_lower_up",           LOWER_UP),
+                FMT_FLAGS("is_dormant",            DORMANT),
+                FMT_FLAGS("is_echo_device",        ECHO),
+# endif
+                    Conditional{false}
+                ),
+#undef  FMT_FLAGS
+#define FMT_STAT(attr) fmt::arg(# attr, interface.stat.attr)
+	            FMT_STAT(rx_packets),
+	            FMT_STAT(tx_packets),
+
+	            fmt::arg("rx_bytes", mem_size_t{interface.stat.rx_bytes}),
+	            fmt::arg("tx_bytes", mem_size_t{interface.stat.tx_bytes}),
+
+	            FMT_STAT(rx_errors),
+	            FMT_STAT(tx_errors),
+	            FMT_STAT(rx_dropped),
+	            FMT_STAT(tx_dropped),
+	            FMT_STAT(multicast),
+	            FMT_STAT(collisions),
+
+	            FMT_STAT(rx_length_errors),
+	            FMT_STAT(rx_over_errors),
+	            FMT_STAT(rx_crc_errors),
+	            FMT_STAT(rx_frame_errors),
+	            FMT_STAT(rx_fifo_errors),
+	            FMT_STAT(rx_missed_errors),
+
+	            FMT_STAT(tx_aborted_errors),
+	            FMT_STAT(tx_carrier_errors),
+	            FMT_STAT(tx_fifo_errors),
+	            FMT_STAT(tx_heartbeat_errors),
+	            FMT_STAT(tx_window_errors),
+
+	            FMT_STAT(rx_compressed),
+	            FMT_STAT(tx_compressed),
+#undef  FMT_STAT
+                fmt::arg("ipv4_addrs", interface.ipv4_addrs_v),
+                fmt::arg("ipv6_addrs", interface.ipv6_addrs_v)
+            );
+
+            if (++i != interfaces.size()) {
+                *out = ' ';
+                ++out;
+            }
+        }
+
+        return out;
+    } else
+        return ctx.out();
+}
+
+static auto parse_limit(fmt::format_parse_context &ctx, std::size_t *limit)
+{
+    auto it = ctx.begin(), end = ctx.end();
+    if (it == end)
+        return it;
+
+    int base = 10;
+    if (*it == '0' && (it + 1) != end && *(it + 1) == 'x')
+        base = 16;
+
+    errno = 0;
+    char *endptr;
+    uintmax_t result = strtoumax(it, &endptr, base);
+    if (errno == ERANGE || result > SIZE_MAX)
+        FMT_THROW(fmt::format_error("invalid format: Integer too big"));
+    if (endptr == end || *endptr != '}')
+        FMT_THROW(fmt::format_error("invalid format: Unterminated '{'"));
+
+    *limit = result;
+
+    return static_cast<decltype(it)>(endptr);
+}
+template <class It>
+static It format_to(It out, int af, const void *addr, std::size_t buffer_sz)
+{
+    char buffer[buffer_sz];
+
+    FMT_ASSERT(
+        inet_ntop(af, addr, buffer, buffer_sz) != nullptr,
+        std::strerror(errno)
+    );
+
+    return fmt::format_to(out, "{}", static_cast<const char*>(buffer));
+}
+
+using ipv4_addrs_formatter = fmt::formatter<swaystatus::ipv4_addrs>;
+
+auto ipv4_addrs_formatter::parse(format_parse_context &ctx) -> format_parse_context_it
+{
+    return parse_limit(ctx, &limit);
+}
+auto ipv4_addrs_formatter::format(const ipv4_addrs &addrs, format_context &ctx) ->
+    format_context_it
+{
+    auto out = ctx.out();
+
+    std::size_t cnt = 0;
+    for (auto &addr: addrs) {
+        out = format_to(out, AF_INET, &addr, INET_ADDRSTRLEN);
+
+        ++cnt;
+        if (cnt == limit)
+            break;
+        if (cnt != addrs.cnt) {
+            *out = ' ';
+            ++out;
+        }
+    }
+
+    return out;
+}
+
+using ipv6_addrs_formatter = fmt::formatter<swaystatus::ipv6_addrs>;
+
+auto ipv6_addrs_formatter::parse(format_parse_context &ctx) -> format_parse_context_it
+{
+    return parse_limit(ctx, &limit);
+}
+auto ipv6_addrs_formatter::format(const ipv6_addrs &addrs, format_context &ctx) ->
+    format_context_it
+{
+    auto out = ctx.out();
+
+    std::size_t cnt = 0;
+    for (auto &addr: addrs) {
+        out = format_to(out, AF_INET6, &addr, INET6_ADDRSTRLEN);
+
+        ++cnt;
+        if (cnt == limit)
+            break;
+        if (cnt != addrs.cnt) {
+            *out = ' ';
+            ++out;
+        }
+    }
+
+    return out;
+}
---
+++ src/networking.hpp
@@ -0,0 +1,146 @@
+#ifndef  __swaystatus_networking_H__
+# define __swaystatus_networking_H__
+
+# include "fmt_config.h"
+
+# include <sys/types.h>
+# include <netinet/in.h>
+# include <netinet/ip.h>
+# include <linux/if_link.h>
+
+# include <cstddef>
+# include <string>
+# include <string_view>
+# include <array>
+
+# include "dep/fmt/include/fmt/format.h"
+
+namespace swaystatus {
+using ipv4_addr = struct in_addr;
+using ipv6_addr = struct in6_addr;
+
+using interface_stats = struct rtnl_link_stats;
+
+struct ip_addrs {};
+/**
+ * It is unlikely for one computer to have more than 8 addresses.
+ */
+struct ipv4_addrs: ip_addrs {
+    using const_iterator = typename std::array<ipv4_addr, 8>::const_iterator;
+
+    std::size_t cnt = 0;
+    std::array<ipv4_addr, 8> array;
+
+    void add(const struct sockaddr *src_addr) noexcept;
+    void reset() noexcept;
+
+    auto begin() const noexcept -> const_iterator;
+    auto end() const noexcept -> const_iterator;
+};
+struct ipv6_addrs: ip_addrs {
+    using const_iterator = typename std::array<ipv6_addr, 8>::const_iterator;
+
+    std::size_t cnt = 0;
+    std::array<ipv6_addr, 8> array;
+
+    void add(const struct sockaddr *src_addr) noexcept;
+    void reset() noexcept;
+
+    auto begin() const noexcept -> const_iterator;
+    auto end() const noexcept -> const_iterator;
+};
+struct Interface {
+    static interface_stats get_empty_stats() noexcept;
+
+    std::string name;
+    unsigned int flags;   /* Flags from SIOCGIFFLAGS */
+
+    interface_stats stat = get_empty_stats();
+
+    ipv4_addrs ipv4_addrs_v;
+    ipv6_addrs ipv6_addrs_v;
+
+    /**
+     * operator == and != are used to find the interface.
+     */
+    bool operator == (std::string_view interface_name) const noexcept;
+    bool operator != (std::string_view interface_name) const noexcept;
+
+    void reset() noexcept;
+};
+
+class Interfaces {
+    std::size_t cnt = 0;
+    /**
+     * It is unlikely for one computer to have more than 8 network interfaces.
+     */
+    std::array<Interface, 8> interfaces;
+
+public:
+    using iterator       = typename std::array<Interface, 8>::iterator;
+    using const_iterator = typename std::array<Interface, 8>::const_iterator;
+
+    Interfaces() = default;
+    ~Interfaces() = default;
+
+    auto operator [] (std::string_view name) noexcept -> iterator;
+
+    bool is_empty() const noexcept;
+    auto size() const noexcept -> std::size_t;
+
+    auto begin() noexcept -> iterator;
+    auto end() noexcept -> iterator;
+
+    auto begin() const noexcept -> const_iterator;
+    auto end() const noexcept -> const_iterator;
+
+    auto cbegin() const noexcept -> const_iterator;
+    auto cend() const noexcept -> const_iterator;
+
+    void clear() noexcept;
+};
+}
+
+template <>
+struct fmt::formatter<swaystatus::Interfaces>
+{
+    using Interfaces = swaystatus::Interfaces;
+
+    using format_parse_context_it = typename format_parse_context::iterator;
+    using format_context_it = typename format_context::iterator;
+
+    std::string_view fmt_str = "";
+
+    auto parse(format_parse_context &ctx) -> format_parse_context_it;
+    auto format(const Interfaces &interfaces, format_context &ctx) -> format_context_it;
+};
+
+template <>
+struct fmt::formatter<swaystatus::ipv4_addrs>
+{
+    using ipv4_addrs = swaystatus::ipv4_addrs;
+
+    using format_parse_context_it = typename format_parse_context::iterator;
+    using format_context_it = typename format_context::iterator;
+
+    std::size_t limit = -1;
+
+    auto parse(format_parse_context &ctx) -> format_parse_context_it;
+    auto format(const ipv4_addrs &addrs, format_context &ctx) -> format_context_it;
+};
+
+template <>
+struct fmt::formatter<swaystatus::ipv6_addrs>
+{
+    using ipv6_addrs = swaystatus::ipv6_addrs;
+
+    using format_parse_context_it = typename format_parse_context::iterator;
+    using format_context_it = typename format_context::iterator;
+
+    std::size_t limit = -1;
+
+    auto parse(format_parse_context &ctx) -> format_parse_context_it;
+    auto format(const ipv6_addrs &addrs, format_context &ctx) -> format_context_it;
+};
+
+#endif
