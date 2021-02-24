ab06bc5b7892e52363834a1daa2388cc7d97c00d
Jiahao XU <Jiahao_XU@outlook.com>
2021-02-24 18:15:03 +1100

    Update help.h: Add doc on default val of --interval
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/help.h
+++ src/help.h
@@ -8,8 +8,9 @@ extern "C" {
 static const char * const help = 
     "Usage: swaystatus [options] configuration_filename\n\n"
     "  --help                    Show help message and exit\n"
-    "  --interval=unsigned_msec  Specify update interval in milliseconds, must be an unsigner"
-    " integer.\n\n";
+    "  --interval=unsigned_msec  Specify update interval in milliseconds, must be an unsigner "
+                                 "integer.\n"
+    "                            By default, the interval is set to 1000 ms.\n\n";
 
 # ifdef __cplusplus
 }
