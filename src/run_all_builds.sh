b0bdf7a82047bbdf6b69dec1b8f389b67b6f70f2
Jiahao XU <Jiahao_XU@outlook.com>
2021-03-09 14:00:01 +1100

    Fix src/run_all_builds.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/run_all_builds.sh
+++ src/run_all_builds.sh
@@ -4,4 +4,4 @@ CPUS=$(nproc)
 
 BUILD_DIR=debug_build/ DEBUG=true make -j $CPUS
 BUILD_DIR=release_build/ make -j $CPUS
-exec BUILD_DIR=release_no_py_build/ PYTHON=false make -j $CPUS
+BUILD_DIR=release_no_py_build/ PYTHON=false exec make -j $CPUS
