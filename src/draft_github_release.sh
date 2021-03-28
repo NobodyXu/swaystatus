dc196ef89a15e013cf3d6530862aaf0ac778ca99
Jiahao XU <Jiahao_XU@outlook.com>
2021-03-28 22:09:09 +1100

    Add src/draft_github_release.sh
    This makes creating new releases easier than ever.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ src/draft_github_release.sh
@@ -0,0 +1,46 @@
+#!/bin/sh -ex
+
+get_libc_name() {
+    libc=$(ldd release_build/swaystatus | grep libc | cut -d '>' -f 2 | sed 's/(.*)$//')
+    output=$($libc 2>&1)
+    if echo "$output" | grep -q 'GNU C Library'; then
+        echo glibc
+    elif echo "$output" | grep -q 'musl libc'; then
+        echo musl
+    else
+        echo unknown libc! >&2
+        exit 1
+    fi
+}
+
+label_postfix="$(arch)-$(uname)-$(get_libc_name)"
+
+tag=$1
+if [ -z "$tag" ]; then
+    echo "Usage: $0 <tag> [Release notes]" >&2
+    exit 1
+fi
+
+if [ -n "$2" ]; then
+    changelogOption="-n"
+    changelogValue="$2"
+else
+    changelogFile=$(mktemp)
+    nvim "$changelogFile"
+
+    changelogOption="-F"
+    changelogValue="$changelogFile"
+fi
+
+./run_all_builds.sh
+
+cp release_build/swaystatus "/tmp/swaystatus-with-python-support-$label_postfix"
+cp release_no_py_build/swaystatus "/tmp/swaystatus-no-python-support-$label_postfix"
+
+git push
+exec gh release create "$tag" \
+    --prerelease \
+    $changelogOption "$changelogValue" \
+    -t "swaystatus $tag" \
+    "/tmp/swaystatus-with-python-support-$label_postfix" \
+    "/tmp/swaystatus-no-python-support-$label_postfix"
