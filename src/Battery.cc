2fe8ddb30247684933bf4766aa88aa0ac0feeb29
Jiahao XU <Jiahao_XU@outlook.com>
2021-04-20 18:12:03 +1000

    Refactor print_battery.cc and create class Battery; Add feature specified in #33
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ src/Battery.cc
@@ -0,0 +1,179 @@
+#ifndef  _GNU_SOURCE
+# define _GNU_SOURCE     /* For strchrnul, strcasestr */
+#endif
+
+#include <cstring>
+
+#include <err.h>
+
+#include <fcntl.h>     /* For O_RDONLY */
+#include <unistd.h>    /* For close, lseek and fstat */
+
+#include <utility>
+
+#include "dep/fmt/include/fmt/core.h"
+
+#include "utility.h"
+#include "fmt_utility.hpp"
+
+#include "Battery.hpp"
+
+namespace swaystatus {
+auto Battery::makeBattery(int path_fd, std::string_view device) -> std::optional<Battery>
+{
+    size_t device_name_len = device.size();
+
+    std::string path(device);
+    path.reserve(device_name_len + 1 + sizeof("uevent"));
+
+    path.append("/type");
+
+    int fd = openat_checked(power_supply_path, path_fd, path.c_str(), O_RDONLY);
+
+    char type[sizeof("Battery")];
+    ssize_t bytes = readall(fd, type, sizeof(type) - 1);
+    if (bytes < 0)
+        err(1, "%s on %s%s failed", "readall", power_supply_path, path.c_str());
+    if (bytes == 0)
+        errx(1, "%s on %s%s failed", "Assumption", power_supply_path, path.c_str());
+    type[bytes] = '\0';
+
+    close(fd);
+
+    if (std::strncmp("Battery", type, sizeof("Battery") - 1) != 0)
+        return {std::nullopt};
+
+    path.resize(device_name_len);
+
+    return Battery{path_fd, std::move(path)};
+}
+
+Battery::Battery(int path_fd, std::string &&device):
+    battery_device{std::move(device)}
+{
+    battery_device.append("/uevent");
+
+    uevent_fd = openat_checked(power_supply_path, path_fd, battery_device.c_str(), O_RDONLY);
+    read_battery_uevent();
+}
+
+Battery::Battery(Battery &&other) noexcept:
+    battery_device{std::move(other.battery_device)},
+    uevent_fd{other.uevent_fd},
+    buffer{std::move(other.buffer)}
+{
+    other.uevent_fd = -1;
+}
+
+Battery::~Battery()
+{
+    if (uevent_fd != -1)
+        close(uevent_fd);
+}
+
+void Battery::read_battery_uevent()
+{
+    buffer.clear();
+
+    ssize_t cnt = asreadall(uevent_fd, buffer);
+    if (cnt < 0)
+        err(1, "%s on %s%s/%s failed", "read", power_supply_path, battery_device.c_str(), "uevent");
+
+    if (lseek(uevent_fd, 0, SEEK_SET) == static_cast<off_t>(-1))
+        err(1, "%s on %s%s/%s failed", "lseek", power_supply_path, battery_device.c_str(), "uevent");
+}
+
+auto Battery::get_property(std::string_view name) const noexcept -> std::string_view
+{
+    if (uevent_fd == -1)
+        return {};
+
+    char *substr = strcasestr(const_cast<char*>(buffer.c_str()), name.data());
+    if (!substr)
+        return "nullptr";
+
+    const char *value = substr + name.size() + 1;
+    const char *end = strchrnul(substr, '\n');
+
+    return {value, static_cast<std::size_t>(end - value)};
+}
+} /* namespace swaystatus */
+
+using Batteries_formatter = fmt::formatter<std::vector<swaystatus::Battery>>;
+
+auto Batteries_formatter::parse(format_parse_context &ctx) -> format_parse_context_it
+{
+    auto it = ctx.begin(), end = ctx.end();
+    if (it == end)
+        return it;
+
+    end = swaystatus::find_end_of_format(ctx);
+
+    fmt_str = std::string_view{it, static_cast<std::size_t>(end - it)};
+
+    return end;
+}
+auto Batteries_formatter::format(const Batteries &batteries, format_context &ctx)
+    -> format_context_it
+{
+    auto out = ctx.out();
+
+    if (fmt_str.size() == 0)
+        return out;
+
+    for (const swaystatus::Battery &battery: batteries) {
+        auto get_bat_property_lazy = [&battery](const char *name) noexcept
+        {
+            return swaystatus::LazyEval{[&battery, name]() noexcept
+            {
+                return battery.get_property(name);
+            }};
+        };
+        auto get_conditional_lazy = [&battery](const char *name, const char *val) noexcept
+        {
+            return swaystatus::LazyEval{[&battery, name, val]() noexcept
+            {
+                return swaystatus::Conditional{battery.get_property(name) == val};
+            }};
+        };
+
+        out = format_to(
+            out,
+
+            fmt_str,
+
+            fmt::arg("type", "battery"),
+
+#define ARG(literal) fmt::arg((literal), get_bat_property_lazy(literal))
+            ARG("name"),
+            ARG("present"),
+            ARG("technology"),
+
+            ARG("model_name"),
+            ARG("manufacturer"),
+            ARG("serial_number"),
+
+            ARG("status"),
+
+            ARG("cycle_count"),
+
+            ARG("voltage_min_design"),
+            ARG("voltage_now"),
+
+            ARG("charge_full_design"),
+            ARG("charge_full"),
+            ARG("charge_now"),
+
+            ARG("capacity"),
+            ARG("capacity_level"),
+#undef ARG
+
+            fmt::arg("is_charging", get_conditional_lazy("status", "Charging")),
+            fmt::arg("is_discharging", get_conditional_lazy("status", "Discharging")),
+            fmt::arg("is_not_charging", get_conditional_lazy("status", "Not charging")),
+            fmt::arg("is_full", get_conditional_lazy("status", "Full"))
+        );
+    }
+
+    return out;
+}
--- src/print_battery.cc
+++ src/print_battery.cc
@@ -1,11 +1,12 @@
-#define _DEFAULT_SOURCE /* For macro constants of struct dirent::d_type and struct timespec */
+#define _DEFAULT_SOURCE /* For macro constants of struct dirent::d_type */
 #ifndef  _GNU_SOURCE
 # define _GNU_SOURCE     /* For strchrnul */
 #endif
 
-#include <stddef.h>
-#include <string.h>
-#include <assert.h>
+#include <cstddef>
+#include <cstring>
+#include <cassert>
+
 #include <err.h>
 
 #include <sys/types.h>
@@ -15,19 +16,16 @@
 #include <unistd.h>    /* For close, lseek and fstat */
 
 #include <string_view>
+#include <vector>
 
 #include "utility.h"
 #include "process_configuration.h"
 #include "printer.hpp"
-#include "Conditional.hpp"
-#include "LazyEval.hpp"
+#include "Battery.hpp"
 #include "print_battery.h"
 
-using swaystatus::Conditional;
-using swaystatus::LazyEval;
+using swaystatus::Battery;
 using swaystatus::print;
-
-static const char * const path = "/sys/class/power_supply/";
 
 static const char *full_text_format;
 static const char *short_text_format;
@@ -35,70 +33,23 @@ static const char *short_text_format;
 static uint32_t cycle_cnt;
 static uint32_t interval;
 
-/**
- * If battery not found, battery_device is left unchanged, which by default is NULL
- *
- * Since having multiple batteries for laptop/desktop/workstation doesn't make much sense,
- * swaystatus now only supports single battery device
- *
- * Android does seem to have multiple batteries, as shown in here:
- * https://stackoverflow.com/questions/26616073/multiple-battery-entries-in-sys-class-power-supply-on-android
- *
- * However, since sway, a tiling wm that heavily depends on keyboard is unlikely
- * to be used on Android, this is not considered to be a problem.
- */
-static const char *battery_device;
-static int uevent_fd;
-
-static char *buffer;
-static size_t buffer_sz;
+static std::vector<Battery> batteries;
 
 extern "C" {
-static void read_battery_uevent();
-static int add_battery(int path_fd, const char *device)
-{
-    battery_device = strdup_checked(device);
-
-    size_t device_name_len = strlen(battery_device);
-    char relative_path[device_name_len + 1 + sizeof("uevent")];
-
-    memcpy(relative_path, device, device_name_len);
-    relative_path[device_name_len] = '/';
-    memcpy(relative_path + device_name_len + 1, "type", sizeof("type"));
-
-    int fd = openat_checked(path, path_fd, relative_path, O_RDONLY);
-
-    char type[sizeof("Battery")];
-    ssize_t bytes = readall(fd, type, sizeof(type) - 1);
-    if (bytes < 0)
-        err(1, "%s on %s%s failed", "readall", path, buffer);
-    if (bytes == 0)
-        errx(1, "%s on %s%s failed", "Assumption", path, buffer);
-    type[bytes] = '\0';
-
-    close(fd);
-
-    if (strncmp("Battery", type, sizeof("Battery") - 1) != 0)
-        return 0;
-
-    memcpy(relative_path + device_name_len + 1, "uevent", sizeof("uevent"));
-    uevent_fd = openat_checked(path, path_fd, relative_path, O_RDONLY);
-    read_battery_uevent();
-
-    return 1;
-}
 void init_battery_monitor(const void *config)
 {
-    full_text_format = get_format(config, "battery", "{has_battery:{status} {capacity}%}");
+    full_text_format = get_format(
+        config,
+        "battery",
+        "{has_battery:{per_battery_fmt_str:{status} {capacity}%}}"
+    );
     short_text_format = get_short_format(config, "battery", NULL);
 
     interval = get_update_interval(config, "battery", 3);
 
-    uevent_fd = -1;
-
-    DIR *dir = opendir(path);
+    DIR *dir = opendir(Battery::power_supply_path);
     if (!dir)
-        err(1, "%s on %s failed", "opendir", path);
+        err(1, "%s on %s failed", "opendir", Battery::power_supply_path);
 
     const int path_fd = dirfd(dir);
 
@@ -107,13 +58,14 @@ void init_battery_monitor(const void *config)
         switch (ent->d_type) {
             case DT_UNKNOWN:
             case DT_LNK:
-                if (!isdir(path, path_fd, ent->d_name))
+                if (!isdir(Battery::power_supply_path, path_fd, ent->d_name))
                     break;
 
             case DT_DIR:
                 if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
-                    if (add_battery(path_fd, ent->d_name))
-                        goto done;
+                    auto result = Battery::makeBattery(path_fd, ent->d_name);
+                    if (result)
+                        batteries.push_back(std::move(*result));
                 }
 
             default:
@@ -122,93 +74,21 @@ void init_battery_monitor(const void *config)
     }
 
     if (errno != 0)
-        err(1, "%s on %s failed", "readdir", path);
+        err(1, "%s on %s failed", "readdir", Battery::power_supply_path);
 
-done:
+    batteries.shrink_to_fit();
+
     closedir(dir);
 }
 
-static void read_battery_uevent()
-{
-    if (uevent_fd == -1)
-        return;
-
-    ssize_t cnt = asreadall(uevent_fd, &buffer, &buffer_sz);
-    if (cnt < 0)
-        err(1, "%s on %s%s/%s failed", "read", path, battery_device, "uevent");
-
-    if (lseek(uevent_fd, 0, SEEK_SET) == (off_t) -1)
-        err(1, "%s on %s failed", "lseek", path);
-}
-
-static auto get_bat_property(std::string_view name) -> std::string_view
-{
-    if (uevent_fd == -1)
-        return {};
-
-    char *substr = strcasestr(buffer, name.data());
-    if (!substr)
-        return "nullptr";
-
-    const char *value = substr + name.size() + 1;
-    const char *end = strchrnul(substr, '\n');
-
-    return {value, static_cast<std::size_t>(end - value)};
-}
-static auto get_bat_property_lazy(std::string_view name) noexcept
-{
-    return LazyEval{[=]() noexcept
-    {
-        return get_bat_property(name);
-    }};
-}
-static auto get_conditional_lazy(std::string_view name, std::string_view val) noexcept
-{
-    return LazyEval{[=]() noexcept
-    {
-        return Conditional{get_bat_property(name) == val};
-    }};
-}
-
-static void print_fmt(const char *name, const char *format)
+static void print_fmt(const char *name, const char *fmt)
 {
     print("\"{}\":\"", name);
 
     print(
-        format,
-
-        fmt::arg("has_battery", Conditional{uevent_fd != -1}),
-
-        fmt::arg("type", "battery"),
-
-#define ARG(literal) fmt::arg((literal), get_bat_property_lazy(literal))
-        ARG("name"),
-        ARG("present"),
-        ARG("technology"),
-
-        ARG("model_name"),
-        ARG("manufacturer"),
-        ARG("serial_number"),
-
-        ARG("status"),
-
-        ARG("cycle_count"),
-
-        ARG("voltage_min_design"),
-        ARG("voltage_now"),
-
-        ARG("charge_full_design"),
-        ARG("charge_full"),
-        ARG("charge_now"),
-
-        ARG("capacity"),
-        ARG("capacity_level"),
-#undef ARG
-
-        fmt::arg("is_charging", get_conditional_lazy("status", "Charging")),
-        fmt::arg("is_discharging", get_conditional_lazy("status", "Discharging")),
-        fmt::arg("is_not_charging", get_conditional_lazy("status", "Not charging")),
-        fmt::arg("is_full", get_conditional_lazy("status", "Full"))
+        fmt, 
+        fmt::arg("has_battery", swaystatus::Conditional{batteries.size() != 0}),
+        fmt::arg("per_battery_fmt_str", batteries)
     );
 
     print_literal_str("\",");
@@ -217,7 +97,8 @@ void print_battery()
 {
     if (++cycle_cnt == interval) {
         cycle_cnt = 0;
-        read_battery_uevent();
+        for (Battery &bat: batteries)
+            bat.read_battery_uevent();
     }
 
     print_fmt("full_text", full_text_format);
