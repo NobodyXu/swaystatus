267dfce06d80bac553c59a290881d98f288bed1a
Jiahao XU <Jiahao_XU@outlook.com>
2021-04-29 14:21:02 +1000

    Add errno checking to detect strftime failure if the libc support using errno to report error
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/print_time.c
+++ src/print_time.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE 200112L /* For localtime_r */
 
 #include <err.h>
+#include <errno.h>
 
 #include <time.h>
 #include <unistd.h>
@@ -20,14 +21,20 @@ void init_time(const void *config)
 
 static void print_fmt(const struct tm *local_time, const char *name, const char *format)
 {
+    errno = 0;
+
     /*
      * allocate a big enough buffer to make sure strftime never fails
      */
     char buffer[4096];
     size_t cnt = strftime(buffer, sizeof(buffer), format, local_time);
-    if (cnt == 0)
-        errx(1, "strftime returns 0: Your format string generate string longer than 4096 "
-                "which is larger than the buffer");
+    if (cnt == 0) {
+        if (errno != 0)
+            err(1, "strftime failed");
+        else
+            errx(1, "strftime returns 0: Your format string generate string longer than 4096 "
+                    "which is larger than the buffer");
+    }
 
     print_literal_str("\"");
     print_str(name);
