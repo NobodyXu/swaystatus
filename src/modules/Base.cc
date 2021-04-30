e058364201f8a23170e75e148acfad7fc587663b
Jiahao XU <Jiahao_XU@outlook.com>
2021-04-30 21:49:59 +1000

    Declare && Impl swaystatus::modules::Base
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ src/modules/Base.cc
@@ -0,0 +1,69 @@
+#include <err.h>
+
+#include <cstdarg>
+#include <cstring>
+
+#include "../process_configuration.h"
+#include "../error_handling.hpp"
+#include "../formatting/printer.hpp"
+
+#include "Base.hpp"
+
+using namespace std::literals;
+
+namespace swaystatus::modules {
+Base::Base(void *config, std::string_view module_name_arg,
+           std::uint32_t default_interval,
+           const char *default_full_format, const char *default_short_format,
+           unsigned n, ...):
+    module_name{module_name_arg},
+    full_text_format{get_format(config, default_full_format)},
+    short_text_format{get_short_format(config, default_short_format)},
+    interval{get_update_interval(config, module_name_arg.data(), default_interval)}
+{
+    std::va_list ap;
+    va_start(ap, n);
+    user_specified_properties_str.reset(get_user_specified_property_str_impl2(config, n, ap));
+    if (user_specified_properties_str)
+        user_specified_properties_str_len = std::strlen(user_specified_properties_str.get());
+    va_end(ap);
+}
+
+void Base::update_and_print()
+{
+    if (++cycle_cnt == interval) {
+        cycle_cnt = 0;
+        update();
+    }
+
+    print_literal_str("{\"name\":\"");
+    print_str2(module_name);
+    print_literal_str("\",\"instance\":\"0\",");
+
+    print_fmt("full_text"sv, full_text_format.get());
+    if (short_text_format)
+        print_fmt("short_text"sv, short_text_format.get());
+
+    if (user_specified_properties_str)
+        print_str2(user_specified_properties_str.get(), user_specified_properties_str_len);
+    else
+        print_literal_str("\"separator\":true");
+
+    print_literal_str("},");
+}
+void Base::print_fmt(std::string_view name, const char *format)
+{
+    print_literal_str("\"");
+    print_str2(name);
+    print_literal_str("\":\"");
+
+    TRY {
+        do_print(format);
+    } CATCH (const std::exception &e) {
+        errx(1, "Failed to print %s format in print_%s.cc: %s",
+                name.data(), module_name.data(), e.what());
+    };
+
+    print_literal_str("\",");
+}
+} /* namespace swaystatus::modules */
---
+++ src/modules/Base.hpp
@@ -0,0 +1,62 @@
+#ifndef  __swaystatus_modules_Base_HPP__
+# define __swaystatus_modules_Base_HPP__
+
+# include <cstdint>
+# include <memory>
+# include <string_view>
+
+# include "../formatting/printer.hpp"
+
+namespace swaystatus::modules {
+class Base {
+    // instance variables
+    const std::string_view module_name;
+
+    const std::unique_ptr<const char[]> full_text_format;
+    const std::unique_ptr<const char[]> short_text_format;
+
+    std::uint32_t cycle_cnt = 0;
+    const std::uint32_t interval;
+
+    std::unique_ptr<const char[]> user_specified_properties_str;
+    std::size_t user_specified_properties_str_len;
+
+    // instance methods
+
+    /**
+     * @param name need to be null-terminated
+     */
+    void print_fmt(std::string_view name, const char *format);
+
+protected:
+    Base() = delete;
+
+    Base(const Base&) = delete;
+    Base(Base&&) = delete;
+
+    Base& operator = (const Base&) = delete;
+    Base& operator = (Base&&) = delete;
+
+    /**
+     * @param module_name_arg should be null-terminated
+     * @param n number of variadic args
+     * @param args should be properties to be removed before converting this module_config 
+     *             the a string `"property0": val, ...`, suitable for printing a json directly
+     *             Eg: "\"border_top\":2,\"borer_left\":3"
+     *
+     * This ctor invokes uses n and args to invoke get_user_specified_property_str_impl2.
+     */
+    Base(void *config, std::string_view module_name_arg,
+         std::uint32_t default_interval,
+         const char *default_full_format, const char *default_short_format,
+         unsigned n, ...);
+
+    virtual void update();
+    virtual void do_print(const char *format);
+
+public:
+    void update_and_print();
+};
+} /* namespace swaystatus::modules */
+
+#endif
