516fb70dbe54b767a09070505dd7ca9170e7dffd
Jiahao XU <Jiahao_XU@outlook.com>
2021-04-30 20:18:56 +1000

    Refactor print_brightness: Create class Backlight
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ src/Backlight.cc
@@ -0,0 +1,73 @@
+#define _DEFAULT_SOURCE /* For macro constants of struct dirent::d_type and struct timespec */
+
+#include <err.h>
+
+#include <fcntl.h>     /* For O_RDONLY */
+#include <unistd.h>    /* For close and lseek */
+
+#include "utility.h"
+
+#include "Backlight.hpp"
+
+namespace swaystatus {
+Backlight::Backlight(int path_fd, const char *filename_arg):
+    filename{filename_arg}
+{
+    auto &buffer = filename;
+    auto filename_sz = filename.size();
+
+    {
+        buffer.append("/max_brightness");
+        int fd = openat_checked(path, path_fd, buffer.c_str(), O_RDONLY);
+        buffer.resize(filename_sz);
+
+        const char *failed_part = readall_as_uintmax(fd, &max_brightness);
+        if (failed_part)
+            err(1, "%s on %s%s/%s failed", failed_part, path, buffer.c_str(), "max_brightness");
+        close(fd);
+    }
+
+    buffer.append("/brightness");
+    this->fd = openat_checked(path, path_fd, buffer.c_str(), O_RDONLY);
+    buffer.resize(filename_sz);
+
+    buffer.shrink_to_fit();
+
+    update_brightness();
+}
+
+Backlight::~Backlight()
+{
+    close(fd);
+}
+
+std::uintmax_t Backlight::calculate_brightness()
+{
+    uintmax_t val;
+    const char *failed_part = readall_as_uintmax(fd, &val);
+    if (failed_part)
+        err(1, "%s on %s%s/%s failed", failed_part, path, filename.c_str(), "brightness");
+
+    if (lseek(fd, 0, SEEK_SET) == (off_t) -1)
+        err(1, "%s on %s%s/%s failed", "lseek", path, filename.c_str(), "brightness");
+
+    return 100 * val / max_brightness;
+}
+void Backlight::update_brightness()
+{
+    brightness = calculate_brightness();
+}
+
+auto Backlight::get_device_name() const noexcept -> std::string_view
+{
+    return filename;
+}
+auto Backlight::get_brightness() const noexcept -> std::uintmax_t
+{
+    return brightness;
+}
+auto Backlight::get_max_brightness() const noexcept -> std::uintmax_t
+{
+    return max_brightness;
+}
+} /* namespace swaystatus */
--- src/modules/print_brightness.cc
+++ src/modules/print_brightness.cc
@@ -13,6 +13,7 @@
 #include <unistd.h>    /* For close and lseek */
 
 #include <exception>
+#include <vector>
 
 #include "../error_handling.hpp"
 #include "../utility.h"
@@ -20,34 +21,17 @@
 #include "../handle_click_events.h"
 #include "../formatting/printer.hpp"
 #include "../formatting/Conditional.hpp"
+#include "../Backlight.hpp"
 #include "print_brightness.h"
 
 using swaystatus::Conditional;
 using swaystatus::print;
 using swaystatus::get_user_specified_property_str;
+using swaystatus::Backlight;
 
 static const char * const module_name = "brightness";
 
-static const char * const path = "/sys/class/backlight/";
-
-struct Backlight {
-    const char *filename;
-    /**
-     * value read from max_brightness
-     */
-    uintmax_t max_brightness;
-    /**
-     * opened file of /sys/class/backlight/{BacklightDevice}/brightness
-     */
-    int fd;
-    /**
-     * cached brightness
-     */
-    uintmax_t brightness;
-};
-
-static struct Backlight *backlights;
-static size_t backlight_sz;
+static std::vector<Backlight> backlights;
 
 static const char *user_specified_properties_str;
 
@@ -58,40 +42,6 @@ static uint32_t cycle_cnt;
 static uint32_t interval;
 
 extern "C" {
-static void update_brightness(struct Backlight *backlight);
-static void addBacklight(int path_fd, const char *filename)
-{
-    ++backlight_sz;
-    reallocate(backlights, backlight_sz);
-
-    struct Backlight *backlight = &backlights[backlight_sz - 1];
-    backlight->filename = strdup_checked(filename);
-
-    size_t filename_sz = strlen(backlight->filename);
-
-    char buffer[filename_sz + 1 + sizeof("max_brightness")];
-
-    /*
-     * Accessed linearly from low addr to high addr, thus no need to worry about
-     * overwritng heap.
-     */
-    memcpy(buffer, backlight->filename, filename_sz);
-    buffer[filename_sz] = '/';
-    memcpy(buffer + filename_sz + 1, "max_brightness", sizeof("max_brightness"));
-
-    int fd = openat_checked(path, path_fd, buffer, O_RDONLY);
-
-    const char *failed_part = readall_as_uintmax(fd, &backlight->max_brightness);
-    if (failed_part)
-        err(1, "%s on %s%s/%s failed", failed_part, path, backlight->filename, "max_brightness");
-    close(fd);
-
-    memcpy(buffer + filename_sz + 1, "brightness", sizeof("brightness"));
-    backlight->fd = openat_checked(path, path_fd, buffer, O_RDONLY);
-
-    update_brightness(backlight);
-}
-
 void init_brightness_detection(void *config)
 {
     full_text_format = get_format(config, "{backlight_device}: {brightness}");
@@ -103,9 +53,9 @@ void init_brightness_detection(void *config)
 
     user_specified_properties_str = get_user_specified_property_str(config);
 
-    DIR *dir = opendir(path);
+    DIR *dir = opendir(Backlight::path);
     if (!dir)
-        err(1, "%s on %s failed", "opendir", path);
+        err(1, "%s on %s failed", "opendir", Backlight::path);
 
     const int path_fd = dirfd(dir);
 
@@ -114,12 +64,12 @@ void init_brightness_detection(void *config)
         switch (ent->d_type) {
             case DT_UNKNOWN:
             case DT_LNK:
-                if (!isdir(path, path_fd, ent->d_name))
+                if (!isdir(Backlight::path, path_fd, ent->d_name))
                     break;
 
             case DT_DIR:
                 if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0)
-                    addBacklight(path_fd, ent->d_name);
+                    backlights.emplace_back(path_fd, ent->d_name);
 
             default:
                 break;
@@ -127,50 +77,34 @@ void init_brightness_detection(void *config)
     }
 
     if (errno != 0)
-        err(1, "%s on %s failed", "readdir", path);
+        err(1, "%s on %s failed", "readdir", Backlight::path);
 
-    if (backlight_sz == 0)
-        errx(1, "No dir is found under %s", path);
+    if (backlights.size() == 0)
+        errx(1, "No dir is found under %s", Backlight::path);
 
     closedir(dir);
-}
-
-static uintmax_t calculate_brightness(struct Backlight *backlight)
-{
-    uintmax_t val;
-    const char *failed_part = readall_as_uintmax(backlight->fd, &val);
-    if (failed_part)
-        err(1, "%s on %s%s/%s failed", failed_part, path, backlight->filename, "brightness");
-
-    if (lseek(backlight->fd, 0, SEEK_SET) == (off_t) -1)
-        err(1, "%s on %s%s/%s failed", "lseek", path, backlight->filename, "brightness");
-
-    return 100 * val / backlight->max_brightness;
-}
-static void update_brightness(struct Backlight *backlight)
-{
-    backlight->brightness = calculate_brightness(backlight);
+    backlights.shrink_to_fit();
 }
 
 static void print_fmt(const char *name, const char *fmt)
 {
     print("\"{}\":\"", name);
 
-    for (size_t i = 0; i != backlight_sz; ++i) {
-        struct Backlight * const backlight = &backlights[i];
-
+    size_t i = 0;
+    for (const Backlight &backlight: backlights) {
         TRY {
             print(
                 fmt,
-                fmt::arg("backlight_device", backlight->filename),
-                fmt::arg("brightness",       backlight->brightness),
-                fmt::arg("has_multiple_backlight_devices", Conditional{backlight_sz != 1})
+                fmt::arg("backlight_device", backlight.get_device_name()),
+                fmt::arg("brightness",       backlight.get_brightness()),
+                fmt::arg("max_brightness",       backlight.get_max_brightness()),
+                fmt::arg("has_multiple_backlight_devices", Conditional{backlights.size() != 1})
             );
         } CATCH (const std::exception &e) {
             errx(1, "Failed to print %s format in print_%s.cc: %s", name, "brightness", e.what());
         };
 
-        if (i + 1 != backlight_sz)
+        if (i++ != backlights.size())
             print_literal_str(" ");
     }
 
@@ -180,8 +114,8 @@ void print_brightness()
 {
     ++cycle_cnt;
     if (cycle_cnt == interval) {
-        for (size_t i = 0; i != backlight_sz; ++i)
-            update_brightness(&backlights[i]);
+        for (Backlight &backlight: backlights)
+            backlight.update_brightness();
         cycle_cnt = 0;
     }
 
