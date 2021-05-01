55755f8a19272b24d74e50bdae29f41ae6843e87
Jiahao XU <Jiahao_XU@outlook.com>
2021-05-01 12:46:56 +1000

    Define macro LIKELY and UNLIKELY in a portable manner in utility.h
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/utility.h
+++ src/utility.h
@@ -7,6 +7,14 @@
 # include <inttypes.h>
 # include <sys/types.h>
 
+# ifdef __GNUC__
+#  define LIKELY(expr) (__builtin_expect((expr), 1))
+#  define UNLIKELY(expr) (__builtin_expect((expr), 0))
+# else
+#  define LIKELY(expr) (expr)
+#  define UNLIKELY(expr) (expr)
+# endif
+
 # ifdef __cplusplus
 extern "C" {
 # endif
