aae2f154ff99e95d3dd1d95524af01f53c17a0c6
Jiahao XU <Jiahao_XU@outlook.com>
2021-05-08 13:45:13 +1000

    Add module name to err msg printed in fmt_throw_impl
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/formatting/fmt_config.cc
+++ src/formatting/fmt_config.cc
@@ -6,10 +6,20 @@
 #include "fmt_config.hpp"
 
 namespace swaystatus {
+static const char *module = nullptr;
+void fmt_set_calling_module(const char *module_arg) noexcept
+{
+#ifndef CXX_HAS_EXCEPTION
+    module = module_arg;
+#endif
+}
+
 [[noreturn]]
 void fmt_throw_impl(const std::exception &e, const char *func, int line, const char *file) noexcept
 {
-    std::fprintf(stderr, "Error when formatting: %s in %s, %d, %s\n", e.what(), func, line, file);
+    std::fprintf(stderr,
+                 "Error when formatting in module %s: %s in %s, %d, %s\n",
+                 module ? module : "nullptr", e.what(), func, line, file);
     stack_bt();
     std::exit(1);
 }
