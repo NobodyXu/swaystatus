bcd910907652c425977c5a218f17f194a537c99a
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-26 17:08:34 +1000

    Update draft_github_release.sh: Better cmdline arg parsing
    Only print usage when no cmdline arg is present or `-h`/`--help` is
    passed.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- draft_github_release.sh
+++ draft_github_release.sh
@@ -19,7 +19,7 @@ cd $(dirname $0)
 
 label_postfix="$(arch)-$(uname)-$(get_libc_name)"
 
-if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+if [ $# -lt 1 ] || [ $# -gt 2 ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "Usage: $0 <tag> [Release notes]" >&2
     exit 1
 fi
